55431ec11e1367d09c3d0dab80db9e44257fe993
Brenley Dueck <brenleydueck@gmail.com>
2026-07-25 00:05:30 -0500

    Simplify

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-/* @refresh reload */
-import { render } from '@solidjs/web'
-import './index.css'
-import App from './App.tsx'
-
-const root = document.getElementById('root')
-
-render(() => <App />, root!)
